32ac483411f1097a96b7262d9e0e627cc3ad2384
bhusalashish <ashish.bhusal.16999@gmail.com>
2021-01-29 16:40:41 +0530

    Added react-page editor

--- client/src/Components/MyEditor.js
+++ client/src/Components/MyEditor.js
@@ -2,10 +2,6 @@ import React, { useState } from "react";
 
 // The editor core
 import Editor from "@react-page/editor";
-
-// Load some exemplary plugins:
-
-// The rich text area plugin
 import slate from "@react-page/plugins-slate";
 // Stylesheets for the rich text area plugin
 import "@react-page/plugins-slate/lib/index.css";
@@ -14,10 +10,6 @@ import "@react-page/plugins-slate/lib/index.css";
 import background from "@react-page/plugins-background";
 // Stylesheets for  background layout plugin
 import "@react-page/plugins-background/lib/index.css";
-// import { simpleDefaultState } from "./Simple-default-state";
-
-// Define which plugins we want to use.
-// We only have slate and background available, so load those.
 const plugins = {
     // Define plugins for content cells.
     // To import multiple plugins, use [slate(), image, spacer, divider]
